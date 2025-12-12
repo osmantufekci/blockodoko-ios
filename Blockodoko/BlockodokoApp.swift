a70e21aa176e4447963e0a69ba52951ad1c51bbb
Osman Tüfekçi <Osman.Tufekci@vodafone.com>
2025-12-12 14:55:48 +0300

    init firebase on app start

--- Blockodoko/BlockodokoApp.swift
+++ Blockodoko/BlockodokoApp.swift
@@ -1,10 +1,23 @@
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct BlockodokoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject var viewModel = GameViewModel()
+
     var body: some Scene {
         WindowGroup {
             MainGameView()
+                .environmentObject(viewModel)
                 .preferredColorScheme(.dark) // Force dark mode as per design
         }
     }
