33548004e05d2774786b4df3d67d206aaa3da6e6
Osman Tüfekçi <Osman.Tufekci@vodafone.com>
2025-12-13 12:23:24 +0300

    request app tracking

--- Blockodoko/BlockodokoApp.swift
+++ Blockodoko/BlockodokoApp.swift
@@ -1,11 +1,33 @@
 import SwiftUI
 import FirebaseCore
+import GoogleMobileAds
+import AppTrackingTransparency
+import AdSupport
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { self.requestTrackingPermission() }
         return true
+    }
+
+    func requestTrackingPermission() {
+        ATTrackingManager.requestTrackingAuthorization { status in
+            // İzin verilse de verilmese de reklam servisini başlat
+            MobileAds.shared.start(completionHandler: nil)
+
+            switch status {
+            case .authorized:
+                print("ATT: İzin Verildi ✅")
+            case .denied, .restricted:
+                print("ATT: İzin Verilmedi ❌")
+            case .notDetermined:
+                print("ATT: Belirsiz")
+            @unknown default:
+                break
+            }
+        }
     }
 }
 
@@ -13,6 +35,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct BlockodokoApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var viewModel = GameViewModel()
+    @StateObject var adsManager = AdsManager.shared
     @StateObject var navigationManager: NavigationManager = .shared
 
     var body: some Scene {
@@ -20,6 +43,7 @@ struct BlockodokoApp: App {
             NavigationStack(path: $navigationManager.path) {
                 MainGameView()
                     .environmentObject(viewModel)
+                    .environmentObject(adsManager)
                     .preferredColorScheme(.dark)
                     .navigationDestination(for: NavigationView<AnyView>.self) { destination in
                         destination
